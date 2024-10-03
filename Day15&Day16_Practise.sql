1ef406743a8c35095a78b537e83e49ccebd6dda8
nguoitinhmamtom <hienhoangle7@gmail.com>
2024-10-04 00:20:30 +0700

    Update Day15&Day16_Practise.sql

--- Day15&Day16_Practise.sql
+++ Day15&Day16_Practise.sql
@@ -156,4 +156,12 @@ WHERE total_weight<=1000
 ORDER BY total_weight DESC
 LIMIT 1
 --8
-
+SELECT product_id, 
+FIRST_VALUE(new_price) OVER(PARTITION BY product_id ORDER BY change_date DESC) AS price
+FROM Products
+WHERE change_date <= '2019-08-16'
+UNION 
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
